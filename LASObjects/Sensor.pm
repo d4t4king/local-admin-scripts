bd54c97c28645e62cc10c402eb6bd622ff938f85
dataking <dataking@gmail.com>
2016-06-24 02:28:50 -0700

    WIP

--- LASObjects/Sensor.pm
+++ LASObjects/Sensor.pm
@@ -5,18 +5,27 @@ package LASObjects::Sensor;
 use strict;
 use warnings;
 
+use Data::Dumper;
 use Digest::SHA qw( sha256_hex );
 
 sub new {
-	my $class = shift(\@_);
+	my $class = shift(@_);
 
-	my $self = (
-		'bus'		=>	shift(@_),
-		'adapter'	=>	shift(@_),
-		'node'		=>	shift(@_),
-		'sensor'	=>	shift(@_),
-		'temp'		=>	shift(@_0)
-	);
+	my $self = {
+		'id'					=>	'',
+		'bus'					=>	shift(@_),
+		'adapter'				=>	shift(@_),
+		'node'					=>	shift(@_),
+		'sensor'				=>	shift(@_),
+		'input_temp'			=>	shift(@_),
+		'max_temp'				=>	0,
+		'crit_temp'				=>	0,
+		'crit_alarm'			=>	0,
+		'crit_max'				=>	0,
+		'crit_histogram'		=>	0,
+		'emerg_max'				=>	0,
+		'emerg_historgram'		=>	0,
+	};
 
 	$self->{'id'} = uc(sha256_hex("$self->{'bus'},$self->{'adapter'},$self->{'node'},$self->{'sensor'}"));
 
