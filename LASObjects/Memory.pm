cd6b80f1e938ca3ce24a68d0d7b07c2c9dffe159
dataking <dataking@gmail.com>
2016-06-25 15:46:16 -0700

    Memory.pm TBD

---
+++ LASObjects/Memory.pm
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+package LASObjects::Memory;
+
+our @ISA		= qw( Exporter );
+our @EXPORT		= qw( );
+our @EXPORT_OK	= qw( );
+our $version	= '0.0.1';
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Term::ANSIColor;
+
+sub new {
+	my $class = shift;
+	
+	my $self = {
+		'total'		= shift(@_),
+		'free'		= shift(@_),
+		'used'		= shift(@_),
+	};
+
+	bless $self, $class;
+
+	return $self;
+}
+
+1;
+
